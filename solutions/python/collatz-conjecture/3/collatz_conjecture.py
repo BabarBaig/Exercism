3a63b4843b5ad5518c3c95f42db9adadc89a5936
Babar B <BobBaig@outlook.com>
2025-10-04 22:34:44 -0400

    Update collatz_conjecture.py

--- solutions/python/collatz-conjecture/3/collatz_conjecture.py
+++ solutions/python/collatz-conjecture/3/collatz_conjecture.py
@@ -3,7 +3,7 @@ def steps(number: int) -> int:
         raise ValueError("Only positive integers are allowed")
     step_count = 0
     while number > 1:
-        # Perform integer division below -- far more efficient that fload division
+        # Perform integer division below -- far more efficient than float division
         number = number // 2 if number % 2 == 0 else number * 3 + 1
         step_count += 1
     return step_count
